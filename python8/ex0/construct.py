e7f2f7bab0084ea26440e33c5b2ea3dd25ef1352
Khalid Abououalid <khabouou@c3r9p8.1337.ma>
2026-04-17 16:05:56 +0100

    fixing some stuff in python8

--- python8/ex0/construct.py
+++ python8/ex0/construct.py
@@ -1,21 +1,29 @@
-import sys 
+import sys
 import os
 
+# current python environment
 prefix = sys.prefix
+
+# always means the global python installs regardless of you run script in venv or not
 base_prefix = sys.base_prefix
+
+# full path of python currently running your script so output depend on from where you run the script
 executable_path = sys.executable
+
+# extract the last parm of prefix so in returns the name of the venv
 venv_name = os.path.basename(prefix)
+
 
 if prefix == base_prefix:
     print("\nMATRIX STATUS: You're still plugged in\n")
-    print(f"Current Python: {prefix}")
+    print(f"Current Python: {executable_path}")
     print(f"Virtual Environment: None detected\n")
     print("WARNING: You're in the global environment!")
     print("The machines can see everything you install.\n")
     print("To enter the construct, run:")
     print("python -m venv matrix_env")
     print("source matrix_env/bin/activate # On Unix")
-    print("matrix_env\Script\activate # On Windows")
+    print(r"matrix_env\Scripts\activate # On Windows")
     print("\nThen run this program again.")
 else:
     print("\nMATRIX STATUS: Welcome to the construct\n")
--- python8/ex1/loading.py
+++ python8/ex1/loading.py
@@ -1,44 +1,46 @@
 import sys
+import importlib.util
+import importlib.metadata
 
+def check_packages(packages: dict) -> None:
 
+    for pkg, description in packages.items():
+        version = importlib.metadata.version(pkg)
+        print(f"[OK] {pkg} ({version}) - {description}")
+
+required_modules = {"numpy": "Numerical computation ready",
+                     "pandas": "Data manipulation ready",
+                       "matplotlib": "Visualization ready",
+                         "requests": "Network access ready"}
 print("\nLOADING STATUS: Loading programs...\n")
 print("Checking dependencies:")
-try:
-    import numpy
-except ImportError as e:
-    print(e)
-    print("Install all required modules and dependencies using: pip install -r requirements.txt or poetry install")
-    exit()
-try:
-    import pandas
-except ImportError as e:
-    print(e)
-    print("Install all required modules and dependencies using: pip install -r requirements.txt or poetry install")
-    exit()
-try:
-    import matplotlib
-except ImportError as e:
-    print(e)
-    print("Install all required modules and dependencies using: pip install -r requirements.txt or poetry install")
-    exit()
-try:
-    import requests
-except ImportError as e:
-    print(e)
-    print("Install all required modules and dependencies using: pip install -r requirements.txt or poetry install")
-    exit()
 
-print(f"[OK] requests ({requests.__version__}) - Network access ready")
-print(f"[OK] matplotlib ({matplotlib.__version__}) - Visualization ready")
-print(f"[OK] pandas ({pandas.__version__}) - Data manipulation ready")
-print(f"[OK] numpy ({numpy.__version__}) - Numerical computation ready")
-
-
+for module in required_modules:
+    if importlib.util.find_spec(module) is None:
+        print(f"[KO] missing package {module}")
+        print("\nInstall dependencies using:")
+        print("pip install -r requirements.txt or poetry install")
+        sys.exit()
+import numpy
+import pandas
+import matplotlib
+import requests
 import matplotlib.pyplot
+check_packages(required_modules)
+
+
+# randint generate 1000 number and sctore them into a numpy array 
 data = numpy.random.randint(0, 100, size=1000)
+
+# so DataFrame create columns and rows of numbers
 dataframe = pandas.DataFrame(data, columns=["MatrixValue"])
 print(f"Processing {len(dataframe)} data points...")
 print("Generating visualization...")
+
+# hist draw a histogram a bar chart that counts how many times numbers appear
+# bins=10 split the number int 10 groups. 0-10 in one bar and so on.
+# alpha=1 1 means fully visible bar, 0 means invisible
+ 
 matplotlib.pyplot.hist(dataframe["MatrixValue"], bins=10, alpha=1)
 matplotlib.pyplot.title("Matrix Data Analysis")
 matplotlib.pyplot.xlabel("Value")
