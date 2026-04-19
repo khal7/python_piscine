dd2d46043647f5778a226f7bfff56b56f152bf48
Khalid Abououalid <khabouou@e1r3p12.1337.ma>
2026-04-19 14:08:15 +0100

    adding python9

---
+++ python9/ex1/alien_contact.py
@@ -0,0 +1,92 @@
+
+
+from enum import Enum
+from datetime import datetime
+from pydantic import BaseModel, Field, ValidationError, model_validator
+from typing import Optional
+
+
+class ContactType(Enum):
+    radio = "radio"
+    visual = "visual"
+    physical = "physical"
+    telepathic = "telepathic"
+
+
+class AlienContact(BaseModel):
+
+    contact_id: str = Field(min_length=5, max_length=15)
+    timestamp: datetime = Field(...)
+    location: str = Field(min_length=3, max_length=100)
+    contact_type: ContactType = Field(ContactType)
+    signal_strength: float = Field(ge=0.0, le=10.0)
+    duration_minutes: int = Field(ge=1, le=1440)
+    witness_count: int = Field(ge=1, le=100)
+    message_received: Optional[str] = Field(max_length=500)
+    is_verified: bool = Field(False)
+
+    @model_validator(mode='after')
+    def validate_contact_rules(self):
+        if not self.contact_id.startswith("AC"):
+            raise ValueError("Contact ID must start with AC")
+        if self.contact_type == ContactType.physical and self.is_verified == False:
+            raise ValueError("Physical contact reports must be verified")
+
+        if self.contact_type == ContactType.telepathic and self.witness_count < 3:
+            raise ValueError(
+                "Telepathic contact requires at least 3 witnesses")
+
+        if self.signal_strength > 7.0 and self.message_received == None:
+            raise ValueError("Strong signals must include a received message")
+        return self
+
+
+if __name__ == "__main__":
+    print("Alien Contact Log Validation")
+    print("======================================")
+    print("Valid contact report:")
+    try:
+        alien_contact = AlienContact(contact_id="AC_2024_001",
+                                    timestamp=datetime(2024, 6, 15, 22, 30, 0),
+                                    location="Area 51, Nevada",
+                                    contact_type=ContactType.radio,
+                                    signal_strength=8.5,
+                                    duration_minutes=45,
+                                    witness_count=5,
+                                    message_received='Greetings from Zeta Reticuli',
+                                    is_verified=False)
+        print(f"ID: {alien_contact.contact_id}")
+        print(f"Type: {alien_contact.contact_type.value}")
+        print(f"Location: {alien_contact.location}")
+        print(f"Signal: {alien_contact.signal_strength}/10")
+        print(f"Duration: {alien_contact.duration_minutes} minutes")
+        print(f"Witnesses: {alien_contact.witness_count}")
+        print(f"Message: '{alien_contact.message_received}'")
+    except ValidationError as e:
+        print(e)
+    print("\n======================================")
+    print("Expected validation error:")
+    try:
+        alien = AlienContact(contact_id="AC_2024_001",
+                                    timestamp=datetime(2024, 6, 15, 22, 30, 0),
+                                    location="Area 51, Nevada",
+                                    contact_type=ContactType.telepathic,
+                                    signal_strength=8.5,
+                                    duration_minutes=45,
+                                    witness_count=2,
+                                    message_received='Greetings from Zeta Reticuli',
+                                    is_verified=False)
+        print(f"ID: {alien.contact_id}")
+        print(f"Type: {alien.contact_type.value}")
+        print(f"Location: {alien.location}")
+        print(f"Signal: {alien.signal_strength}/10")
+        print(f"Duration: {alien.duration_minutes} minutes")
+        print(f"Witnesses: {alien.witness_count}")
+        print(f"Message: '{alien.message_received}'")
+    except ValidationError as e:
+        for error in e.errors():
+            print(error['ctx']['error'])
+    
+
+
+    
---
+++ python9/ex2/space_crew.py
@@ -0,0 +1,117 @@
+from enum import Enum
+from datetime import datetime
+from pydantic import BaseModel, Field, ValidationError, model_validator
+from typing import Optional
+
+
+class Rank(Enum):
+    cadet = "cadet"
+    officer = "officer"
+    lieutenant = "lieutenant"
+    captain = "captain"
+    commander = "commander"
+
+
+class CrewMember(BaseModel):
+
+    number_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=2, max_length=50)
+    rank: Rank = Field(...)
+    age: int = Field(ge=18, le=80)
+    specialization: str = Field(min_length=3, max_length=30)
+    years_experience: int = Field(ge=0, le=50)
+    is_active: bool = Field(True)
+
+
+class SpaceMission(BaseModel):
+    mission_id: str = Field(min_length=5, max_length=15)
+    mission_name: str = Field(min_length=3, max_length=100)
+    destination: str = Field(min_length=3, max_length=50)
+    launch_date: datetime = Field(...)
+    duration_days: int = Field(ge=1, le=3650)
+    crew: list[CrewMember] = Field(min_length=1, max_length=12)
+    mission_status: str = Field('planned')
+    budget_millions: float = Field(ge=1.0, le=10000.0)
+
+    @model_validator(mode='after')
+    def rule_validation(self):
+        if not self.mission_id.startswith("M"):
+            raise ValueError("something")
+        if not any(m.rank in [Rank.commander, Rank.captain] for m in self.crew):
+            raise ValueError("Must have at least one Commander or Captain")
+
+        if self.duration_days > 365:
+            experienced = sum(1 for m in self.crew if m.years_experience >= 5)
+            if experienced / len(self.crew) < 0.5:
+                raise ValueError("Long missions need 50% experienced crew")
+        if not all(m.is_active for m in self.crew):
+            raise ValueError("All crew members must be active")
+        return self
+
+
+if __name__ == "__main__":
+    print("Space Mission Crew Validation")
+    print("=========================================")
+    print("Valid mission created:")
+    print("Mission: Mars Colony Establishment")
+    try:
+        sarah = CrewMember(number_id="S1_00",
+                        name="Sarah Connor",
+                        rank=Rank.commander,
+                        age=20,
+                        specialization="Mission Command",
+                        years_experience=5,
+                        is_active=True
+                        )
+        john = CrewMember(number_id="S2_00",
+                        name="John Smith",
+                        rank=Rank.lieutenant,
+                        age=20,
+                        specialization="Navigation",
+                        years_experience=7,
+                        is_active=True
+                        )
+        alice = CrewMember(number_id="S3_00",
+                        name="Alice Johnson",
+                        rank=Rank.officer,
+                        age=20,
+                        specialization="Engineering",
+                        years_experience=9,
+                        is_active=True
+                        )
+        crew_members = [sarah,john, alice]
+        mission = SpaceMission(mission_id="M2024_MARS",
+                            mission_name="Mars Colony Establishment",
+                            destination="Mars",
+                            launch_date=datetime(2024, 6, 15, 22, 30, 0),
+                            duration_days=900,
+                            crew=crew_members,
+                            mission_status='planned',
+                            budget_millions=2500.0,
+                            )
+    except ValidationError as e:
+        print(e)
+    print(f"ID: {mission.mission_id}")
+    print(f"Desitnation: {mission.destination}")
+    print(f"Duration: {mission.duration_days} days")
+    print(f"Budget: ${mission.budget_millions}M")
+    print(f"Crew size: {len(mission.crew)}")
+    for member in mission.crew:
+        print(f"- {member.name} ({member.rank.value}) - {member.specialization}")
+    print("\n=========================================")
+    print("Expected validation error:")
+    try:
+        bad_crew = [
+            CrewMember(number_id="S1_00", name="Sarah Connor", rank=Rank.cadet, age=20, specialization="Mission Command", years_experience=5, is_active=True),
+        ]
+        SpaceMission(mission_id="M2024_MARS",
+                    mission_name="Mars Colony Establishment",
+                    destination="Mars",
+                    launch_date=datetime(2024, 6, 15, 22, 30, 0),
+                    duration_days=900,
+                    crew=bad_crew,
+                    mission_status='planned',
+                    budget_millions=2500.0)
+    except ValidationError as e:
+        for error in e.errors():
+            print(error['ctx']['error'])
